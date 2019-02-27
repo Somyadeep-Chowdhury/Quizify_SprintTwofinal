c74d4a5e0e7b1ed88c10674a15e9f0e9befdb264
somycho1 <soumyadeep123456@gmail.com>
2019-02-27 12:24:38 +0530

    sprint 2 quizify final commit

---
+++ Quizify-FrontEnd/src/app/quizify/tsclasses/register.ts
@@ -0,0 +1,12 @@
+
+export class Register {
+
+    userName: string;
+    password: string;
+    confirmPassword: string;
+    emailId: string;
+    interests: string;
+    // geners: string;
+    gender: string;
+
+}
---
+++ recommendation-service/src/main/java/com/stackroute/quizify/recommendationservice/controller/TopicController.java
@@ -0,0 +1,30 @@
+package com.stackroute.quizify.recommendationservice.controller;
+
+import com.stackroute.quizify.recommendationservice.domain.Topics;
+import com.stackroute.quizify.recommendationservice.service.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rest/neo4j/topic")
+public class TopicController {
+
+    private TopicService topicService;
+
+    @Autowired
+    public TopicController(TopicService topicService) {
+        this.topicService = topicService;
+    }
+
+    @GetMapping("/{categoryId}")
+    public List<Topics> getAll(@PathVariable("categoryId") long categoryId){
+        return topicService.getAll(categoryId);
+    }
+}
+
+
